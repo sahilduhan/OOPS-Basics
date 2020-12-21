ed2e7f57ed4a00e523d5aa7c58877dfa23cd73c3
sahilduhan <sahilduhan16092001@gmail.com>
2020-12-22 00:52:01 +0530

    update files

--- oops4.cpp
+++ oops4.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Player
+{
+private:
+    string name{"xxxxx"};
+    int health;
+    int xp;
+
+public:
+    Player(std::string name = "sahil", int health = 100, int xp = 50);
+};
+Player(std::string name_val, int health_val, int xp_val) : name(name_val), health(health_val), xp(xp + val)
+{
+    cout << "three args constructor" << endl;
+}
 
 int main()
 {
+
+    Player frank{"frank"};
+    Player villan{"villan"};
     return 0;
 }
