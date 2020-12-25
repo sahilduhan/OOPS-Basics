c62c5bfb096a10a49c919c5278150c3dcb5656e3
sahilduhan <sahilduhan16092001@gmail.com>
2020-12-26 01:17:29 +0530

    oops update

--- oops4.cpp
+++ oops4.cpp
@@ -3,14 +3,14 @@ using namespace std;
 class Player
 {
 private:
-    string name{"xxxxx"};
+    std::string name{"xxxxx"};
     int health;
     int xp;
 
 public:
     Player(std::string name = "sahil", int health = 100, int xp = 50);
 };
-Player(std::string name_val, int health_val, int xp_val) : name(name_val), health(health_val), xp(xp_val)
+Player::Player(string name_val, int health_val, int xp_val) : name{name_val}, health{health_val}, xp{xp_val}
 {
     cout << "three args constructor" << endl;
 }
@@ -20,5 +20,6 @@ int main()
 
     Player frank{"frank"};
     Player villan{"villan"};
+    Player sahil{"sahil",100,50};
     return 0;
 }
