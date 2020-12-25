35f47084de5898eb37edcfceeba2818632376b28
sahilduhan <sahilduhan16092001@gmail.com>
2020-12-26 00:26:22 +0530

    update oops

--- oops4.cpp
+++ oops4.cpp
@@ -10,7 +10,7 @@ private:
 public:
     Player(std::string name = "sahil", int health = 100, int xp = 50);
 };
-Player(std::string name_val, int health_val, int xp_val) : name(name_val), health(health_val), xp(xp + val)
+Player(std::string name_val, int health_val, int xp_val) : name(name_val), health(health_val), xp(xp_val)
 {
     cout << "three args constructor" << endl;
 }
