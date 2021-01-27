a8a0dbbfa3dde87ab494694f8c10f365ef50fb2a
sahilduhan <sahilduhan16092001@gmail.com>
2021-01-28 00:40:53 +0530

    update oops

--- oops_basics/static_again.cpp
+++ oops_basics/static_again.cpp
@@ -1,11 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+class Human
+{
 
-
-
+private:
+public:
+    static int human_count;
+    Human()
+    {
+        human_count++;
+    }
+    void display_active()
+    {
+        cout << human_count << " is the number of active humans " << endl;
+    }
+};
+int Human::human_count = 0;
 
 int main()
 {
+    Human anil;
+
+    anil.display_active();
 
     return 0;
 }
