33f0657957c8eadeceae873e4bfc81cda626f3ff
Sachin Duhan <78694755+duhan-sachin@users.noreply.github.com>
2021-03-06 01:51:39 +0530

    update

--- inheritance/constructor_inheritance_2.cpp
+++ inheritance/constructor_inheritance_2.cpp
@@ -2,11 +2,41 @@
 using namespace std;
 class Base
 {
+    int value;
 
-    
+public:
+    Base() : value{0}
+    {
+        cout << " no args constructor" << endl;
+    }
+    Base(int x) : value{x}
+    {
+        cout << " constructor with single args " << endl;
+    }
+};
+class Derived : public Base
+{
+private:
+    int double_value;
+
+public:
+    Derived() : Base{}, double_value{0}
+    {
+        cout << " derived no args constructor is called" << endl;
+    }
+    Derived(int x) : Base{x}, double_value{x * 2}
+    {
+        cout << " derived with int as a contructor" << endl;
+    }
+    ~Derived()
+    {
+        cout << " the derived destructor is called" << endl;
+    }
 };
 int main()
 {
+    Derived d;
+    d.~Derived();
 
     return 0;
 }
